194b3ebb8868bd67a299c73f09c5840c25ef4680
jfessard <julian.fessard@gmail.com>
2018-01-05 02:21:28 -0800

    Added Monoprice HDX-401TA IR control for input switching
    Also it now flashes faster.

--- src/esproj.cpp
+++ src/esproj.cpp
@@ -2,6 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <fauxmoESP.h>
 #include <SoftwareSerial.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
 
 // Wifi parameters
 
@@ -10,25 +12,55 @@ char ssid[]       = "***";
 
 char rbuf[64] = {0};
 
+
+#define PROJECTOR_PWR   0
+#define CHROMECAST_IN   1
+#define FIRETV_IN       2
+#define CONSOLE_IN      3
+#define PC_IN           4
+
+/* Monoprice HDX-401TA remote codes (NEC, 32 bits) */
+#define SWITCH_IR_1     0xFF10EF //chromecast
+#define SWITCH_IR_2     0xFF50AF //firetv
+#define SWITCH_IR_3     0xFF30CF //console
+#define SWITCH_IR_4     0xFF708F //PC
+#define SWITCH_IR_MUTE  0xFF40BF
+#define SWITCH_IR_LEFT  0xFF08F7
+#define SWITCH_IR_RIGHT 0xFF48B7
+#define SWITCH_IR_2CH   0xFF18E7
+#define SWITCH_IR_51CH  0xFF58A7
+#define SWITCH_IR_POWER 0xFF00FF
+
+// Pin assignments
+IRsend  irsend(D3);  // IR LED control
 #define PROJ_TX D2 //To projector
 #define PROJ_RX D1
+
 #define SERIAL_BAUDRATE                 115200
 #define PROJ_BAUDRATE                   9600
 #define LED                             2
 
 fauxmoESP fauxmo;
+
 SoftwareSerial SerialP(PROJ_RX, PROJ_TX); // RX, TX
 
 char pon[]  = {0x2, 'P', 'O', 'N', 0x3};
 char poff[] = {0x2, 'P', 'O', 'F', 0x3};
 char menu[] = {0x2, 'O', 'M', 'N', 0x3};
 char back[] = {0x2, 'O', 'B', 'K', 0x3};
-
 char qpw[]  = {0x2, 'Q', 'P', 'W', 0x3}; // query power state
 
-// -----------------------------------------------------------------------------
-// Wifi
-// -----------------------------------------------------------------------------
+uint32_t codes[] = {SWITCH_IR_1, SWITCH_IR_2, SWITCH_IR_3, SWITCH_IR_4};
+
+void switch_control(unsigned char device) {
+  device--;
+  Serial.printf("%s: Setting Input %d: IR[0x%x]\n", __func__, device, codes[device]);
+  irsend.sendNEC(codes[device], 32);
+  delay(500);
+  Serial.print("Sending AUDIO 2CH keypress\n");
+  irsend.sendNEC(SWITCH_IR_2CH, 32); //Force stereo
+}
+
 
 void wifiSetup() {
   // Set WIFI module to STA mode
@@ -68,32 +100,53 @@ void setup() {
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
 
+  // IR
+  irsend.begin();
+
   // You can enable or disable the library at any moment
   // Disabling it will prevent the devices from being discovered and switched
   fauxmo.enable(true);
 
-  // Add virtual devices
-  fauxmo.addDevice("projector");
+  // Add virtual devices (order matters)
+  fauxmo.addDevice("projector"); //Panasonic PT-AE3000U
+  fauxmo.addDevice("chromecast");
+  fauxmo.addDevice("firetv");  // TRIGGERS input 1
+  fauxmo.addDevice("console");
+  fauxmo.addDevice("pc input"); // TRIGGERS input 3
 
   // fauxmoESP 2.0.0 has changed the callback signature to add the device_id, this WARRANTY
   // it's easier to match devices to action without having to compare strings.
   fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state) {
+
     Serial.printf("\n[MAIN] Device #%d (%s) state: %s\n", device_id, device_name, state ? "ON" : "OFF");
-    if (state) {
-      Serial.write("SENDING:");
-      for (int i = 0; i < 5; i++) {
-        SerialP.write(pon[i]);
-        Serial.write(pon[i]);
-        delay(300);
-      }
-    } else {
-      for (int i = 0; i < 5; i++) {
-        SerialP.write(poff[i]);
-        Serial.write(poff[i]);
-        delay(300);
-      }
+
+    switch(device_id) {
+      case PROJECTOR_PWR:
+        if (state) {
+          for (int i = 0; i < 5; i++) { // clunky but works.
+            SerialP.write(pon[i]);
+            Serial.write(pon[i]);
+            delay(300);
+          }
+        } else {
+          for (int i = 0; i < 5; i++) {
+            SerialP.write(poff[i]);
+            Serial.write(poff[i]);
+            delay(300);
+          }
+        }
+        digitalWrite(LED, !state);
+        break;
+      case FIRETV_IN:
+      case CHROMECAST_IN:
+      case CONSOLE_IN:
+      case PC_IN:
+        if(state)
+          switch_control(device_id);
+        else
+          irsend.sendNEC(SWITCH_IR_2, 32); /* Back to firetv whenever they're turned off*/
+        break;
     }
-    digitalWrite(LED, !state);
   });
 
 }
